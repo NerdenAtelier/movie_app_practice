ef56da6b1409326a08a8a959be98a9cd9a69ab04
NerdenAtelier <joonjae.kr@gmail.com>
2022-02-01 01:53:59 +0900

    until right before navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Home from './routes/Home';
+import About from './routes/About';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-    return <Home />;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' exact={true} element={<Home />} />
+                <Route path='/about' element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
